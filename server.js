a49297712ee432bb582edadc073635b826ab7719
Fullbuster <Fullbuster0033@gmail.com>
2018-03-16 18:56:37 +0200

    upgrage for heroku

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.Port || 8080);
+// app.use(express.static(__dirname + '/dist'));
+// app.listen(process.env.Port || 8080);
+
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+  console.log("Listening on Port 3000");
+});
